996c21ecf3ea8539738a08f50b01b38e4e4e3b4f
Edouard Mercier <register@edouardmercier.fr>
2012-05-07 14:39:34 +0200

    Changes the default title for some new document types.

--- src/main/java/org/nuxeo/anatole/listener/TitleInitializerListener.java
+++ src/main/java/org/nuxeo/anatole/listener/TitleInitializerListener.java
@@ -8,37 +8,67 @@ import org.nuxeo.ecm.core.event.EventContext;
 import org.nuxeo.ecm.core.event.EventListener;
 import org.nuxeo.ecm.core.event.impl.DocumentEventContext;
 
-public class TitleInitializerListener implements EventListener {
+public class TitleInitializerListener
+    implements EventListener
+{
 
-    @Override
-    public void handleEvent(Event event) throws ClientException {
-        if (DocumentEventTypes.ABOUT_TO_CREATE.equals(event.getName())) {
-            EventContext ctx = event.getContext();
-            if (ctx instanceof DocumentEventContext) {
-                DocumentEventContext docCtx = (DocumentEventContext) ctx;
-                DocumentModel doc = docCtx.getSourceDocument();
-                String title = (String) doc.getPropertyValue("dc:title");
-                if (title == null || title.isEmpty()) {
-                    setDefaultTitle(doc);
-                }
-            }
+  @Override
+  public void handleEvent(Event event)
+      throws ClientException
+  {
+    if (DocumentEventTypes.ABOUT_TO_CREATE.equals(event.getName()))
+    {
+      EventContext ctx = event.getContext();
+      if (ctx instanceof DocumentEventContext)
+      {
+        DocumentEventContext docCtx = (DocumentEventContext) ctx;
+        DocumentModel doc = docCtx.getSourceDocument();
+        String title = (String) doc.getPropertyValue("dc:title");
+        if (title == null || title.isEmpty())
+        {
+          setDefaultTitle(doc);
         }
+      }
     }
+  }
 
-    protected void setDefaultTitle(DocumentModel doc) throws ClientException {
-        String title = "";
-        if (doc.getType().equals("alaune")) {
-            title = "A la une";
-        } else if (doc.getType().equals("whatDoIDoToday")) {
-            title = "Je fais quoi, aujourd’hui ?";
-        } else if (doc.getType().equals("anatoleFranceTour")) {
-            title = "Le Tour de France d'Anatole";
-        } else if (doc.getType().equals("todaysChallenge")) {
-            title = "Le défi du jour";
-        } else if (doc.getType().equals("whosThatPerson")) {
-            title = "Qui c’est celui-là ?";
-        }
-        doc.setPropertyValue("dc:title", title);
+  protected void setDefaultTitle(DocumentModel doc)
+      throws ClientException
+  {
+    String title = "";
+    if (doc.getType().equals("alaune"))
+    {
+      title = "À la une";
     }
+    else if (doc.getType().equals("whatDoIDoToday"))
+    {
+      title = "Je fais quoi, aujourd'hui ?";
+    }
+    else if (doc.getType().equals("anatoleFranceTour"))
+    {
+      title = "Le Tour de France d'Anatole";
+    }
+    else if (doc.getType().equals("todaysChallenge"))
+    {
+      title = "Le défi du jour";
+    }
+    else if (doc.getType().equals("whosThatPerson"))
+    {
+      title = "Qui c'est celui-là ?";
+    }
+    else if (doc.getType().equals("whosThatPerson"))
+    {
+      title = "Qui c'est celui-là ?";
+    }
+    else if (doc.getType().equals("anatolesAgenda"))
+    {
+      title = "Les rendez-vous d'Anatole";
+    }
+    else if (doc.getType().equals("anatolesIdeas"))
+    {
+      title = "Les idées d'Anatole";
+    }
+    doc.setPropertyValue("dc:title", title);
+  }
 
 }
