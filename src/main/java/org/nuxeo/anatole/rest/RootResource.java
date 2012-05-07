a8645cce1371417a4dfd9c5a93fa951233e67db2
Edouard Mercier <register@edouardmercier.fr>
2012-05-07 18:13:57 +0200

    Customizing the export web service.

--- src/main/java/org/nuxeo/anatole/rest/RootResource.java
+++ src/main/java/org/nuxeo/anatole/rest/RootResource.java
@@ -6,9 +6,13 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonGenerator;
+import org.nuxeo.anatole.Constants;
 import org.nuxeo.anatole.adapter.Page;
 import org.nuxeo.anatole.adapter.PageAdapter;
 import org.nuxeo.ecm.automation.server.jaxrs.io.writers.JsonDocumentListWriter;
+import org.nuxeo.ecm.automation.server.jaxrs.io.writers.JsonDocumentWriter;
 import org.nuxeo.ecm.core.api.DocumentModelList;
 import org.nuxeo.ecm.webengine.model.WebObject;
 import org.nuxeo.ecm.webengine.model.impl.ModuleRoot;
@@ -20,29 +24,42 @@ import org.nuxeo.ecm.webengine.model.impl.ModuleRoot;
  */
 @WebObject(type = "anatoleRoot")
 @Path("/anatole")
-public class RootResource extends ModuleRoot {
+public class RootResource
+    extends ModuleRoot
+{
 
-    public final String[] schemas = { "dublincore", "almanachSection" };
+  public final String[] schemas = { "dublincore", "almanachSection" };
 
-    @GET
-    @Path("ping")
-    public String getPong() {
-        return "pong";
+  @GET
+  @Path("ping")
+  public String getPong()
+  {
+    return "pong";
+  }
+
+  @GET
+  @Path("{date}")
+  public String getPage(@PathParam(value = "date")
+  String date)
+      throws Exception
+  {
+    Page page = PageAdapter.find(getContext().getCoreSession(), date);
+    if (page != null)
+    {
+      // yerk : should do better !!!
+      DocumentModelList articles = page.getArticles();
+      ByteArrayOutputStream out = new ByteArrayOutputStream();
+      final JsonGenerator jsonGenerator = new JsonFactory().createJsonGenerator(out);
+      jsonGenerator.writeStartObject();
+      jsonGenerator.writeFieldName("day");
+      JsonDocumentWriter.writeDocument(jsonGenerator, page.getDocument(), new String[] { Constants.PAGE_TYPE });
+      jsonGenerator.writeFieldName("sections");
+      JsonDocumentListWriter.writeDocuments(jsonGenerator, articles, schemas);
+      jsonGenerator.writeEndObject();
+      jsonGenerator.close();
+      return new String(out.toByteArray());
     }
-
-    @GET
-    @Path("{date}")
-    public String getPage(@PathParam(value = "date")
-    String date) throws Exception {
-        Page page = PageAdapter.find(getContext().getCoreSession(), date);
-        if (page != null) {
-            // yerk : should do better !!!
-            DocumentModelList articles = page.getArticles();
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            JsonDocumentListWriter.writeDocuments(out, articles, schemas);
-            return new String(out.toByteArray());
-        }
-        return "not-found";
-    }
+    return "not-found";
+  }
 
 }
